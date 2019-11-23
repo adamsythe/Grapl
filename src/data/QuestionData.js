895627851d1c5a174a1ce9690909a391c54e7108
Simone van Buuren <simone@Simones-MacBook-Pro.local>
2019-11-23 14:47:36 +0200

    Updated the bios :taco:

--- src/data/QuestionData.js
+++ src/data/QuestionData.js
@@ -14,42 +14,42 @@ export const exampleQuestionData = {
 			description: 'Life Coach and Psychologist',
 			avatar: expert1,
 			id: '0',
-			opinion: 'This is an extremely dangerous view to hold, that makes life very complicated for both men and women'
+			opinion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. \n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 		},
 		{
 			name: 'Expert 2',
 			description: 'Academic Researcher in Social Science',
 			avatar: expert2,
 			id: '1',
-			opinion: 'this is a bad idea'
+			opinion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 		},
 		{
 			name: 'Expert 3',
 			description: 'Sex Therapist and Researcher',
 			avatar: expert3,
 			id: '2',
-			opinion: 'Nisl condimentum id venenatis a condimentum vitae. Morbi tristique senectus et netus et. Mollis aliquam ut porttitor leo a diam. Nunc sed id semper risus in hendrerit. Mattis molestie a iaculis at erat pellentesque. Condimentum id venenatis a condimentum vitae sapien pellentesque. Nibh tortor id aliquet lectus proin nibh nisl. Nunc consequat interdum varius sit amet mattis. Id venenatis a condimentum vitae sapien pellentesque. Cras pulvinar mattis nunc sed blandit. Consectetur adipiscing elit pellentesque habitant morbi tristique. Fermentum iaculis eu non diam phasellus vestibulum lorem sed. Dolor sit amet consectetur adipiscing elit ut aliquam. Habitant morbi tristique senectus et netus et malesuada fames ac.',
+			opinion: 'Nisl condimentum id venenatis a condimentum vitae. Morbi tristique senectus et netus et. Mollis aliquam ut porttitor leo a diam. Nunc sed id semper risus in hendrerit. Mattis molestie a iaculis at erat pellentesque. Condimentum id venenatis a condimentum vitae sapien pellentesque. Nibh tortor id aliquet lectus proin nibh nisl. Nunc consequat interdum varius sit amet mattis. Id venenatis a condimentum vitae sapien pellentesque. Cras pulvinar mattis nunc sed blandit. Consectetur adipiscing elit pellentesque habitant morbi tristique. Fermentum iaculis eu non diam phasellus vestibulum lorem sed. Dolor sit amet consectetur adipiscing elit ut aliquam. Habitant morbi tristique senectus et netus et malesuada fames ac. \n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 		},
 		{
 			name: 'Expert 4',
 			description: 'Technical Manager',
 			avatar: expert4,
 			id: '3',
-			opinion: 'no comment',
+			opinion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. \n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 		},
 		{
 			name: 'Expert 5',
 			description: 'Motivational Speaker',
 			avatar: expert5,
 			id: '4',
-			opinion: 'no comment',
+			opinion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. \n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 		},
 		{
 			name: 'Expert 6',
 			description: 'Psychologist With 10 Years Experience',
 			avatar: expert6,
 			id: '5',
-			opinion: 'no comment',
+			opinion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. \n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 		},
 	],
 }
