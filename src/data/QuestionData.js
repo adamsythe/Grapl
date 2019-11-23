8b81edcdb4dfcf0bd700841e4b325233ee3812f8
Simone van Buuren <simone@Simones-MacBook-Pro.local>
2019-11-23 14:42:20 +0200

    Updated fields for experts :taco:

--- src/data/QuestionData.js
+++ src/data/QuestionData.js
@@ -11,42 +11,42 @@ export const exampleQuestionData = {
 	experts: [
 		{
 			name: 'Expert 1',
-			description: 'over 10 years experience dating women',
+			description: 'Life Coach and Psychologist',
 			avatar: expert1,
 			id: '0',
 			opinion: 'This is an extremely dangerous view to hold, that makes life very complicated for both men and women'
 		},
 		{
 			name: 'Expert 2',
-			description: 'psychologist with 10 years experience treating diabetes',
+			description: 'Academic Researcher in Social Science',
 			avatar: expert2,
 			id: '1',
 			opinion: 'this is a bad idea'
 		},
 		{
 			name: 'Expert 3',
-			description: 'psychologist with 10 years experience treating diabetes',
+			description: 'Sex Therapist and Researcher',
 			avatar: expert3,
 			id: '2',
 			opinion: 'Nisl condimentum id venenatis a condimentum vitae. Morbi tristique senectus et netus et. Mollis aliquam ut porttitor leo a diam. Nunc sed id semper risus in hendrerit. Mattis molestie a iaculis at erat pellentesque. Condimentum id venenatis a condimentum vitae sapien pellentesque. Nibh tortor id aliquet lectus proin nibh nisl. Nunc consequat interdum varius sit amet mattis. Id venenatis a condimentum vitae sapien pellentesque. Cras pulvinar mattis nunc sed blandit. Consectetur adipiscing elit pellentesque habitant morbi tristique. Fermentum iaculis eu non diam phasellus vestibulum lorem sed. Dolor sit amet consectetur adipiscing elit ut aliquam. Habitant morbi tristique senectus et netus et malesuada fames ac.',
 		},
 		{
 			name: 'Expert 4',
-			description: 'psychologist with 10 years experience treating diabetes',
+			description: 'Technical Manager',
 			avatar: expert4,
 			id: '3',
 			opinion: 'no comment',
 		},
 		{
 			name: 'Expert 5',
-			description: 'psychologist with 10 years experience treating diabetes',
+			description: 'Motivational Speaker',
 			avatar: expert5,
 			id: '4',
 			opinion: 'no comment',
 		},
 		{
 			name: 'Expert 6',
-			description: 'zodiac reader',
+			description: 'Psychologist With 10 Years Experience',
 			avatar: expert6,
 			id: '5',
 			opinion: 'no comment',
