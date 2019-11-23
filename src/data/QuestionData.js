0dc4c537ca8bd969e6a088cec9c00463931685fc
Simone van Buuren <simone@Simones-MacBook-Pro.local>
2019-11-23 14:32:02 +0200

    Updated expert images :taco:

--- src/data/QuestionData.js
+++ src/data/QuestionData.js
@@ -1,48 +1,53 @@
-import adam from '../../assets/adam.jpg'
+import expert1 from '../../assets/experts/expert_1.jpeg'
+import expert2 from '../../assets/experts/expert_2.jpg'
+import expert3 from '../../assets/experts/expert_3.jpg'
+import expert4 from '../../assets/experts/expert_4.jpg'
+import expert5 from '../../assets/experts/expert_5.jpeg'
+import expert6 from '../../assets/experts/expert_6.jpg'
 
 export const exampleQuestionData = {
 	id: 0,
 	question: 'Is it okay if a mans girlfreind has had more sexual partners than he has?',
 	experts: [
 		{
-			name: 'adam',
+			name: 'Expert 1',
 			description: 'over 10 years experience dating women',
-			avatar: adam,
+			avatar: expert1,
 			id: '0',
 			opinion: 'This is an extremely dangerous view to hold, that makes life very complicated for both men and women'
 		},
 		{
-			name: 'arnold',
+			name: 'Expert 2',
 			description: 'psychologist with 10 years experience treating diabetes',
-			avatar: adam,
+			avatar: expert2,
 			id: '1',
 			opinion: 'this is a bad idea'
 		},
 		{
-			name: 'arnold',
+			name: 'Expert 3',
 			description: 'psychologist with 10 years experience treating diabetes',
-			avatar: adam,
+			avatar: expert3,
 			id: '2',
 			opinion: 'Nisl condimentum id venenatis a condimentum vitae. Morbi tristique senectus et netus et. Mollis aliquam ut porttitor leo a diam. Nunc sed id semper risus in hendrerit. Mattis molestie a iaculis at erat pellentesque. Condimentum id venenatis a condimentum vitae sapien pellentesque. Nibh tortor id aliquet lectus proin nibh nisl. Nunc consequat interdum varius sit amet mattis. Id venenatis a condimentum vitae sapien pellentesque. Cras pulvinar mattis nunc sed blandit. Consectetur adipiscing elit pellentesque habitant morbi tristique. Fermentum iaculis eu non diam phasellus vestibulum lorem sed. Dolor sit amet consectetur adipiscing elit ut aliquam. Habitant morbi tristique senectus et netus et malesuada fames ac.',
 		},
 		{
-			name: 'arnold',
+			name: 'Expert 4',
 			description: 'psychologist with 10 years experience treating diabetes',
-			avatar: adam,
+			avatar: expert4,
 			id: '3',
 			opinion: 'no comment',
 		},
 		{
-			name: 'arnold',
+			name: 'Expert 5',
 			description: 'psychologist with 10 years experience treating diabetes',
-			avatar: adam,
+			avatar: expert5,
 			id: '4',
 			opinion: 'no comment',
 		},
 		{
-			name: 'arnold',
+			name: 'Expert 6',
 			description: 'zodiac reader',
-			avatar: adam,
+			avatar: expert6,
 			id: '5',
 			opinion: 'no comment',
 		},
